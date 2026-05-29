39eb36e1ce3858863df805e1af66238b9b326bdc
myla_00 <74766120+MylaStrev@users.noreply.github.com>
2026-05-29 22:11:10 +0100

    Create pup_arch_setup.sh

---
+++ setup_scripts/arch/pup_arch_setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+PACKAGES=(
+  "flatpak"
+  "vlc"
+  "blender"
+  "obsidian"
+  "obs-studio"
+  "spotify-launcher"
+  "discord"
+#  "filezilla"
+  "orca-slicer"
+  "protonup-qt"
+  "protonplus"
+  "libreoffice-fresh"
+)
+
+FLATPAK_PACKAGES=(
+  "io.edcd.EDMarketConnector"
+)
+
+echo "## INSTALLING YAY..."
+sudo pacman -S --noconfirm yay
+
+echo "## INSTALLING PACMAN PACKAGES"
+
+for pkg in "${PACKAGES[@]}"; do
+    if pacman -Qi "$pkg" &> /dev/null; then
+        echo "## $pkg is already installed, skipping..."
+    else
+        echo "## installing $pkg..."
+        sudo yay -S --needed --noconfirm "${PACKAGES[@]}"
+    fi
+done
+
+for pkg in "${FLATPAK_PACKAGES[@]}"; do
+    if flatpak info "$pkg" >/dev/null 2>&1; then
+        echo "## $pkg is already installed, skipping..."
+    else
+        echo "## installing $pkg..."
+        sudo flatpak install flathub "${FLATPAK_PACKAGES[@]}" -y --noninteractive
+    fi
+done
+
+echo "## RUNNING UPDATE AND UPGRADE..."
+sudo yay -Syu
+sudo flatpak update
+
+echo "## ALL DONE"
